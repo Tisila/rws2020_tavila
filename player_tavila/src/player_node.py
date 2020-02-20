ea311c0c98e661987f0a448838824aa89696001c
Tisila <tiagoavila44@gmail.com>
2020-02-20 13:58:03 +0000

    Minor cosmetic change
    Just testing pulling respos.

--- player_tavila/src/player_node.py
+++ player_tavila/src/player_node.py
@@ -10,7 +10,23 @@ class Player:
         red_team = rospy.get_param('red_team')
         blue_team = rospy.get_param('blue_team')
         green_team = rospy.get_param('green_team')
-        print("Teams available:\n{}\n{}\n{}".format(red_team[0], blue_team[0], green_team[0]))
+        print("Teams available:\nred: {}\nblue: {}\ngreen: {}".format(red_team[0], blue_team[0], green_team[0]))
+
+        if self.player_name in red_team:
+            self.my_team, self.prey_team, self.hunter_team = 'red', 'green', 'blue'
+            self.my_players, self.preys, self.hunters = red_team, green_team, blue_team
+        elif self.player_name in green_team:
+            self.my_team, self.prey_team, self.hunter_team = 'green', 'blue', 'red'
+            self.my_players, self.preys, self.hunters = green_team, blue_team, red_team
+        elif self.player_name in blue_team:
+            self.my_team, self.prey_team, self.hunter_team = 'blue', 'red', 'green'
+            self.my_players, self.preys, self.hunters = blue_team, red_team, green_team
+        else:
+            rospy.logerr("My name ({}) is not in any team, I want to play!".format(self.player_name))
+            exit(0)
+
+        rospy.logwarn("I am {} and I am on the {} team. {} players are going to die".format(self.player_name, self.my_team, self.prey_team))
+        rospy.loginfo("I am afraid of them {}".format(self.hunters))
 
 
 def callback(msg):
