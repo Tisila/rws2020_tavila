1c7eb60a116d843ee59debe99fe99523ed0e1669
Tisila <tiagoavila44@gmail.com>
2020-02-20 11:44:22 +0000

    Completed checkpoint

--- player_tavila/src/player_node.py
+++ player_tavila/src/player_node.py
@@ -1,6 +1,30 @@
 #!/usr/bin/python
-def main():
-    print("Hello there {}".format('tavila'))
+import rospy
+from std_msgs.msg import String
 
-if __name__=='__main__':
+
+class Player:
+    def __init__(self, player_name):
+        self.player_name = player_name
+        rospy.logwarn("I am {}".format(self.player_name))
+        red_team = rospy.get_param('red_team')
+        blue_team = rospy.get_param('blue_team')
+        green_team = rospy.get_param('green_team')
+        print("Teams available:\n{}\n{}\n{}".format(red_team[0], blue_team[0], green_team[0]))
+
+
+def callback(msg):
+    # rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+    print("received a message containing string {}".format(msg.data))
+
+
+def main():
+    name = 'tavila'
+    rospy.init_node(name, anonymous=False)
+    player = Player(name)
+    rospy.Subscriber("chatter", String, callback)
+    rospy.spin()
+
+
+if __name__ == '__main__':
     main()
