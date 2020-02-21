dcc0776a813fd443b34c8fa331e9232709834ecc
Tisila <tiagoavila44@gmail.com>
2020-02-21 11:12:42 +0000

    Added example code to node.
    I'm a cat now.

--- player_tavila/src/player_node.py
+++ player_tavila/src/player_node.py
@@ -53,24 +53,80 @@ class Player:
 
     def callback_make_a_play(self, msg):
         # print("[MakeAPlay] the speed of cat is {}".format(msg.cat))
-        self.max_vel = msg.turtle
+        self.max_vel = msg.cat
         # Make a play decision making
-        velocity = random.uniform(0, self.max_vel)
-        angle = random.uniform(-self.max_angle, self.max_angle)
-        print("[{}] going to turn {} degrees at {} speed".format(self.player_name, angle, velocity))
-        self.move(self.transform, velocity, angle)
+        velocity = self.max_vel
 
-    def move(self, transform_now, vel, angle):
+        max_vel, max_angle = msg.turtle, math.pi / 30
 
-        if angle > self.max_angle:
-            angle = self.max_angle
-        elif angle < -self.max_angle:
-            angle = -self.max_angle
+        if msg.red_alive:  # PURSUIT MODE: Follow any blue player (only if there is at least one blue alive)
+            target = msg.red_alive[0]  # select the first alive blue player (I am hunting blue)
+            distance, angle = self.get_distance_and_angle_to_target(self.listener, self.player_name, target)
+            if angle is None:
+                angle = 0
+            vel = max_vel  # full throttle
+        else:  # what else to do? Lets just move towards the center
+            target = 'world'
+            distance, angle = self.get_distance_and_angle_to_target(self.listener, self.player_name, target)
+            vel = max_vel  # full throttle
 
-        if vel > self.max_vel:
-            vel = self.max_vel
+        # Actually move the player
+        self.move_player(self.br, self.player_name, self.transform, vel, angle, velocity)
+
+    def get_distance_and_angle_to_target(self, tf_listener, my_name, target_name,
+                                    time=rospy.Time(0), max_time_to_wait=1.0):
+        try:
+            tf_listener.waitForTransform(my_name, target_name, time, rospy.Duration(max_time_to_wait))
+            (trans, rot) = tf_listener.lookupTransform(my_name, target_name, time)
+        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException, tf.Exception):
+            rospy.logwarn(my_name + ': Could not get transform from ' + my_name + ' to ' + target_name)
+            return None, None
+
+        # compute distance and angle
+        x, y = trans[0], trans[1]
+        distance = math.sqrt(x ** 2 + y ** 2)
+        angle = math.atan2(y, x)
+        return distance, angle
+
+    def randomize_player_pose(self, transform, arena_radius=8):
+        """
+        Randomizes the initial pose of a player. Based on the code by MGomes.
+        :param transform: a geometry_msgs.msg.Transform() which will have the values of x,y and yaw randomized.
+        :param arena_radius: the radius of the arena inside which the player can be positioned.
+        """
+        initial_r = arena_radius * random.random()
+        initial_theta = 2 * math.pi * random.random()
+        initial_x = initial_r * math.cos(initial_theta)
+        initial_y = initial_r * math.sin(initial_theta)
+        initial_rotation = 2 * math.pi * random.random()
+        transform.translation.x = initial_x
+        transform.translation.y = initial_y
+        q = tf.transformations.quaternion_from_euler(0, 0, initial_rotation)
+        transform.rotation = Quaternion(q[0], q[1], q[2], q[3])
+
+    def move_player(self, tf_broadcaster, player_name, transform_now, vel, angle, max_vel):
+        """
+        Moves a player given its currrent pose, a velocity, and angle, and a maximum velocity
+        :param tf_broadcaster: Used to publish the new pose of the player
+        :param player_name:  string with the name of the player (must coincide with the name of the tf frame_id)
+        :param transform_now: a geometry_msgs.msg.Transform() containing the current pose. This variable is updated with
+                              the new player pose
+        :param vel: velocity of displacement to take in x axis
+        :param angle: angle to turn, limited by max_angle (pi/30)
+        :param max_vel: maximum velocity or displacement based on the selected animal
+        """
+        max_angle = math.pi / 30
+
+        if angle > max_angle:
+            angle = max_angle
+        elif angle < -max_angle:
+            angle = -max_angle
+
+        if vel > max_vel:
+            vel = max_vel
 
         T1 = transform_now
+
         T2 = Transform()
         T2.rotation = tf.transformations.quaternion_from_euler(0, 0, angle)
         T2.translation.x = vel
@@ -82,7 +138,7 @@ class Player:
                                                            T2.rotation[1],
                                                            T2.rotation[2],
                                                            T2.rotation[3]))
-        matrixT2 = numpy.matmul(matrix_trans, matrix_rot)
+        matrixT2 = np.matmul(matrix_trans, matrix_rot)
 
         matrix_trans = tf.transformations.translation_matrix((T1.translation.x,
                                                               T1.translation.y,
@@ -92,19 +148,19 @@ class Player:
                                                            T1.rotation.y,
                                                            T1.rotation.z,
                                                            T1.rotation.w))
-        matrixT1 = numpy.matmul(matrix_trans, matrix_rot)
+        matrixT1 = np.matmul(matrix_trans, matrix_rot)
 
-        matrix_new_transform = numpy.matmul(matrixT2, matrixT1)
+        matrix_new_transform = np.matmul(matrixT1, matrixT2)
 
         quat = tf.transformations.quaternion_from_matrix(matrix_new_transform)
         trans = tf.transformations.translation_from_matrix(matrix_new_transform)
 
-        self.transform.rotation = Quaternion(quat[0], quat[1], quat[2], quat[3])
-        self.transform.translation.x = trans[0]
-        self.transform.translation.y = trans[1]
-        self.transform.translation.z = trans[2]
+        T1.rotation = Quaternion(quat[0], quat[1], quat[2], quat[3])
+        T1.translation.x = trans[0]
+        T1.translation.y = trans[1]
+        T1.translation.z = trans[2]
 
-        self.br.sendTransform(trans, quat, rospy.Time.now(), self.player_name, "world")
+        tf_broadcaster.sendTransform(trans, quat, rospy.Time.now(), player_name, "world")
 
 
 def callback(msg):
